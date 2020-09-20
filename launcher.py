3e4fd5bd2c4c399f3f99802ca9b47716e138761c
shubhaankar <shubhaankar@hotmail.com>
2020-09-20 13:28:26 +0530

    HEROKU CONFIG

--- launcher.py
+++ launcher.py
@@ -1,5 +1,12 @@
 from lib.bot import bot
+import os
 
 VERSION = "0.0.6"
+try:
+    if os.environ["HEROKU"]=='True':
+        print("Creating config file")
+        os.system("python create-config.py")
+except:
+    pass
 
 bot.run(VERSION)
